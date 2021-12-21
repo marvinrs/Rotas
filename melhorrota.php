444de010a142ca0565cbd45d0c28a913a6d38bec
marvinrs <72471673+marvinrs@users.noreply.github.com>
2021-12-21 19:02:15 -0300

    Definido os destinos finais de acordo
     com o passado via $_GET. Criado o for em javascript que percorre a iteração até o número de vezes de endereços passados via $_GET.

--- melhorrota.php
+++ melhorrota.php
@@ -1,51 +1,121 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Mostra a Melhor Rota</title>
-
-    <?php
-      $endereco_inicial=str_replace(" ", "", "Avenida Dr. Gastão Vidigal, 1132 - Vila Leopoldina")
-      $endereco_thiago=$_GET["endereco"];
-      $endereco_fernando=$_GET["endereco"];
-      $endereco_marcelo=$_GET["endereco"];
-      $endereco_andre=$_GET["endereco"];
-
-      //Transforma o endereco em uma string do PHP e mostra na tela a lat e lng
-      // ini_set("allow_url_fopen", 1);
-        $json = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.$endereco.'&key=AIzaSyD7u0NnHbPwL5vOjrfX8-cL_aiioPftVLc');
-        $data = json_decode($json, true);
-
-        //echo $data['results'][0]['geometry']['location']['lat']; // -1.400045
-        //echo $data['results'][0]['geometry']['location']['lng']; // -48.488917
-    ?>
-
+    <title>Distance Matrix Service</title>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
     <link rel="stylesheet" type="text/css" href="./style.css" />
-    <script>
-        // Inicializa e adiciona o mapa.
-        function initMap() {
-          // A localização de do endereço do usuário
-          const localizacao = { lat: <?php echo $data['results'][0]['geometry']['location']['lat']; ?>, lng: <?php echo $data['results'][0]['geometry']['location']['lng']; ?> };
-          // The map, centralizado em localizacao
-          const map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 17,
-            center: localizacao,
-          });
-          // O marcador, posicionado no endereço informado
-          const marker = new google.maps.Marker({
-            position: localizacao,
-            map: map,
-          });
-        }
-    </script>
+    <script src="./index.js"></script>
+    <?php
+      //Define o endereço inicial
+      $endereco_inicial=str_replace(" ", "", "Avenida Dr. Gastão Vidigal, 1132 - Vila Leopoldina");
+      // Cria as variaveis $destino1, $destino2, etc, de acordo com o número de endereços passados via $_GET.
+      for ($i=1; $i<=count($_GET); $i++){
+        ${"destino$i"} = $_GET["endereco$i"];
+      }
+      //echo $destino1;
+      //echo $destino4;
+      ?>
   </head>
   <body>
-    <h3>Endereço no mapa</h3>
-    <!--O elemento div do mapa -->
-    <div id="map"></div>
+    <script>
+      function initMap() {
+      const bounds = new google.maps.LatLngBounds();
+      const markersArray = [];
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 55.53, lng: 9.4 },
+        zoom: 10,
+      });
+      // initialize services
+      const geocoder = new google.maps.Geocoder();
+      const service = new google.maps.DistanceMatrixService();
+      // cria requisicao
+      const origin1 = "<?php echo $endereco_inicial; ?>";
+      for (var i = 1; i <= <?php echo count($_GET);?>; i++) {
+        const destination 'Hello, ${name}';
+      }
+      const destinationA = "Stockholm, Sweden";
+      const destinationB = "";
+      const request = {
+        origins: [origin1],
+        destinations: [destinationA, destinationB],
+        travelMode: google.maps.TravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.METRIC,
+        avoidHighways: false,
+        avoidTolls: false,
+      };
+    
+      // put request on page
+      document.getElementById("request").innerText = JSON.stringify(
+        request,
+        null,
+        2
+      );
+      // get distance matrix response
+      service.getDistanceMatrix(request).then((response) => {
+        // put response
+        document.getElementById("response").innerText = JSON.stringify(
+          response,
+          null,
+          2
+        );
+    
+        // show on map
+        const originList = response.originAddresses;
+        const destinationList = response.destinationAddresses;
+    
+        deleteMarkers(markersArray);
+    
+        const showGeocodedAddressOnMap = (asDestination) => {
+          const handler = ({ results }) => {
+            map.fitBounds(bounds.extend(results[0].geometry.location));
+            markersArray.push(
+              new google.maps.Marker({
+                map,
+                position: results[0].geometry.location,
+                label: asDestination ? "D" : "O",
+              })
+            );
+          };
+          return handler;
+        };
+    
+        for (let i = 0; i < originList.length; i++) {
+          const results = response.rows[i].elements;
+    
+          geocoder
+            .geocode({ address: originList[i] })
+            .then(showGeocodedAddressOnMap(false));
+    
+          for (let j = 0; j < results.length; j++) {
+            geocoder
+              .geocode({ address: destinationList[j] })
+              .then(showGeocodedAddressOnMap(true));
+          }
+        }
+      });
+    }
+    
+    function deleteMarkers(markersArray) {
+      for (let i = 0; i < markersArray.length; i++) {
+        markersArray[i].setMap(null);
+      }
+    
+      markersArray = [];
+    }
+    </script>
+    <div id="container">
+      <div id="map"></div>
+      <div id="sidebar">
+        <h3 style="flex-grow: 0">Request</h3>
+        <pre style="flex-grow: 1" id="request"></pre>
+        <h3 style="flex-grow: 0">Response</h3>
+        <pre style="flex-grow: 1" id="response"></pre>
+      </div>
+    </div>
 
     <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
     <script
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7u0NnHbPwL5vOjrfX8-cL_aiioPftVLc&callback=initMap&libraries=&v=weekly"
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7u0NnHbPwL5vOjrfX8-cL_aiioPftVLc&callback=initMap&v=weekly"
       async
     ></script>
   </body>
